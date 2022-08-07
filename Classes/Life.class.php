04cbe2a23fdcab9a59ed3f3c33732a684516d91e
Hassan Tarawally <contact@htarawally.co.uk>
2022-08-07 15:13:29 +0100

    Update Life.class.php

--- Classes/Life.class.php
+++ Classes/Life.class.php
@@ -1,20 +1,24 @@
 <?php
-	// Abstract class that provides everything 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+	// Abstract class that provides everything
 	// needed for anything that is day to day life
 
 	abstract class Life extends SavedItem {
 		private $day;
 		private $month;
 		private $year;
-		
+
 		public function __construct($id, $day, $month, $year, $updated) {
 			parent::__construct($id, $updated);
-			
+
 			$this->setDay($day);
 			$this->setMonth($month);
 			$this->setYear($year);
 		} // end constructor
-		
+
 		public final function setDay($day) {
 			if(preg_match("/^[0-9]{1,2}$/", $day)) { // if day contains a number 1 or 2 digits long
 				if($day >= 1 && $day <= 31) {
@@ -24,7 +28,7 @@
 			}
 			else throw new InvalidArgumentException("The day should be in the format d or dd");
 		} // end function setDay
-		
+
 		public final function setMonth($month) {
 			if(preg_match("/^[0-9]{1,2}$/", $month)) { // if month contains a number 1 or 2 digits long
 				if($month >= 1 && $month <= 12) {
@@ -34,22 +38,22 @@
 			}
 			else throw new InvalidArgumentException("The month should be in the format m or mm");
 		} // end function setMonth
-		
+
 		public final function setYear($year) {
 			if(preg_match("/^[0-9]{4}$/", $year)) { // if the year is 4 digits long
 				$this->year = $year;
 			}
 			else throw new InvalidArgumentException("The year should be in the format yyyy");
 		} // end function setYear
-		
+
 		public final function returnDay() {
 			return $this->day;
 		} // end function returnDay
-		
+
 		public final function returnMonth() {
 			return $this->month;
 		} // end function returnMonth
-		
+
 		public final function returnYear() {
 			return $this->year;
 		} // end function returnYear
