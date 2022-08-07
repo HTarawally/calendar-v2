86744cb22171b12c9a81e1c59b2160e21a02cd07
Hassan Tarawally <contact@htarawally.co.uk>
2022-08-07 15:13:49 +0100

    Update Money.class.php

--- Classes/Money.class.php
+++ Classes/Money.class.php
@@ -1,23 +1,27 @@
 <?php
-	// Abstract class that provides everything 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+	// Abstract class that provides everything
 	// needed for anything to do with payments or work
 
 	abstract class Money extends Life {
 		private $catID; // category id used to identify a company or payment category
-		
+
 		public function __construct($id, $day, $month, $year, $category, $updated) {
 			parent::__construct($id, $day, $month, $year, $updated);
-			
+
 			$this->setCatID($category);
 		} // end constructor
-		
+
 		public final function setCatID($catID) {
 			if(preg_match("/^[0-9]{1,}$/", $catID)) { // if catID is an integer
 				$this->catID = $catID;
 			}
 			else throw new InvalidArgumentException("Category ID should be an integer value");
 		} // end function setCatID
-		
+
 		public final function returnCatID() {
 			return $this->catID;
 		} // end function returnCatID
