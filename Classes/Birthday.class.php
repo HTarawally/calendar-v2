8a82d605cd862d6b8e4db9d4c93b1ed80b694997
Hassan Tarawally <contact@htarawally.co.uk>
2022-08-07 15:11:59 +0100

    Update Birthday.class.php

--- Classes/Birthday.class.php
+++ Classes/Birthday.class.php
@@ -1,20 +1,24 @@
 <?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
 	// Birthday class that encapsulates the data for people's birthdays
 	// modifies, inserts, deletes and creates new birthdays
 
 	class Birthday extends Life implements iDatabaseTasks {
 		private $name;
-		
+
 		public function __construct($id, $name, $day, $month, $year, $updated) {
 			parent::__construct($id, $day, $month, $year, $updated);
-			
+
 			$this->setName($name);
 		} // end constructor
-		
+
 		public function setName($name) {
 			if(is_string($name)) {
 				$name = trim(htmlentities(strip_tags($name), ENT_QUOTES));
-				
+
 				if(strlen($name) < 255 && strlen($name) >= 2) {
 					$this->name = $name;
 				}
@@ -22,11 +26,11 @@
 			}
 			else throw new InvalidArgumentException("The person's name should be a string value");
 		} // end function setName
-		
+
 		public function returnName() {
 			return $this->name;
 		} // end function returnName
-		
+
 		public function insertIntoDatabase() {
 			$name = $this->returnName();
 			$day = $this->returnDay();
@@ -35,28 +39,28 @@
 
 			$query = "INSERT INTO birthdays (PersonName, Day, Month, Year)
 				VALUES ('$name', '$day', '$month', '$year')";
-				
+
 			return Calendar::$MYSQLI->query($query);
 		} // end function insertIntoDatabase
-		
+
 		public function updateField() {
 			$ID = $this->returnID();
 			$name = $this->returnName();
 			$day = $this->returnDay();
 			$month = $this->returnMonth();
 			$year = $this->returnYear();
-			
-			$query = "UPDATE birthdays SET PersonName = '$name', Day = '$day', Month = '$month', Year = '$year' 
+
+			$query = "UPDATE birthdays SET PersonName = '$name', Day = '$day', Month = '$month', Year = '$year'
 				WHERE BirthdayID = '$ID'";
-			
+
 			return Calendar::$MYSQLI->query($query);
 		} // end function updateField
-		
+
 		public function deleteField() {
 			$ID = $this->returnID();
-			
+
 			$query = "DELETE FROM birthdays WHERE BirthdayID = '$ID'";
-			
+
 			return Calendar::$MYSQLI->query($query);
 		} // end function deleteField
 	}
