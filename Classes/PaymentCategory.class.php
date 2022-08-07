0f35eac0906a47f280e40813b7948f1072f97ab6
Hassan Tarawally <contact@htarawally.co.uk>
2022-08-07 15:14:27 +0100

    Update PaymentCategory.class.php

--- Classes/PaymentCategory.class.php
+++ Classes/PaymentCategory.class.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 	// PaymentCategory class that encapsulates the data for payment categories
 	// modifies, inserts, deletes and creates new payment categories
 
@@ -6,30 +10,30 @@
 		public function __construct($id, $name, $updated) {
 			parent::__construct($id, $name, $updated);
 		} // end constructor
-		
+
 		public function insertIntoDatabase() {
 			$name = $this->returnCatName();
 
 			$query = "INSERT INTO paymentCategories (Name)
 				VALUES ('$name')";
-			
+
 			return Calendar::$MYSQLI->query($query);
 		} // end function insertIntoDatabase
-		
+
 		public function updateField() {
 			$name = $this->returnCatName();
 			$ID = $this->returnID();
-			
+
 			$query = "UPDATE paymentCategories SET Name = '$name' WHERE PaymentCategoryID = '$ID'";
-				
+
 			return Calendar::$MYSQLI->query($query);
 		} // end function updateField
-		
+
 		public function deleteField() {
 			$ID = $this->returnID();
-			
+
 			$query = "DELETE FROM paymentCategories WHERE PaymentCategoryID = '$ID'";
-			
+
 			return Calendar::$MYSQLI->query($query);
 		} // end function deleteField
 	}
