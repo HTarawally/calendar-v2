70db6876d7ed5f47907e8be4e3d9a3821ad8d3a2
Hassan Tarawally <contact@htarawally.co.uk>
2022-08-07 15:14:57 +0100

    Update Reminder.class.php

--- Classes/Reminder.class.php
+++ Classes/Reminder.class.php
@@ -1,24 +1,28 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 	// Reminder class that encapsulates the data for any reminders
 	// modifies, inserts, deletes and creates new reminder
 
 	class Reminder extends Life implements iDatabaseTasks {
 		private $comment; // string containing the reminder
 		private $occurrence; // how often i should be reminded. 1 = Once only, 2 = yearly, 3 = monthly, 4 = weekly, 5 = daily
-		private $times; // how many times i should be reminded. -1 = forever. 
-		
+		private $times; // how many times i should be reminded. -1 = forever.
+
 		public function __construct($id, $comment, $day, $month, $year, $occ, $times, $updated) {
 			parent::__construct($id, $day, $month, $year, $updated);
-			
+
 			$this->setComment($comment);
 			$this->setOccurrence($occ);
 			$this->setTimes($times);
 		} // end constructor
-		
+
 		public function setComment($comment) {
 			if(is_string($comment)) {
 				$comment = trim(htmlentities(strip_tags($comment), ENT_QUOTES));
-				
+
 				if(strlen($comment) < 255 && strlen($comment) >= 2) {
 					$this->comment = $comment;
 				}
@@ -26,67 +30,67 @@
 			}
 			else throw new InvalidArgumentException("The comment should be a string value");
 		} // end function setComment
-		
+
 		public function setOccurrence($occ) {
 			if($occ > 0 && $occ <= 6) {
-				$this->occurrence = $occ;	
+				$this->occurrence = $occ;
 			}
 			else throw new InvalidArgumentException("Occurrence should be an integer between 1 and 6 inclusive");
 		} // end function setOccurrence
-		
+
 		public function setTimes($times) {
 			if($times >= -1 && $times != 0) {
 				$this->times = $times;
 			}
 			else throw new InvalidArgumentException("Times should either be -1 or a positive integer");
 		} // end function setTimes
-		
+
 		public function returnComment() {
-			return $this->comment;	
+			return $this->comment;
 		} // end function returnComment
-		
+
 		public function returnOccurrence() {
 			return $this->occurrence;
 		} // end function returnOccurrence
-		
+
 		public function returnTimes() {
-			return $this->times;	
+			return $this->times;
 		} // end function returnTimes
-		
+
 		public function insertIntoDatabase() {
 			$comment = $this->returnComment();
 			$day = $this->returnDay();
-			$month = $this->returnMonth(); 
+			$month = $this->returnMonth();
 			$year = $this->returnYear();
 			$occurrence = $this->returnOccurrence();
 			$times = $this->returnTimes();
 
 			$query = "INSERT INTO reminders (Comment, Day, Month, Year, Occurrence, Times)
 				VALUES ('$comment', '$day', '$month', '$year', '$occurrence', '$times')";
-				
+
 			return CALENDAR::$MYSQLI->query($query) ;
 		} // end function insertIntoDatabase
-		
+
 		public function updateField() {
 			$ID = $this->returnID();
 			$comment = $this->returnComment();
 			$day = $this->returnDay();
-			$month = $this->returnMonth(); 
+			$month = $this->returnMonth();
 			$year = $this->returnYear();
 			$occurrence = $this->returnOccurrence();
 			$times = $this->returnTimes();
-			
-			$query = "UPDATE reminders SET Comment = '$comment', Day = '$day', Month = '$month', Year = '$year', Occurrence = '$occurrence', 
+
+			$query = "UPDATE reminders SET Comment = '$comment', Day = '$day', Month = '$month', Year = '$year', Occurrence = '$occurrence',
 				Times='$times' WHERE ReminderID = '$ID'";
-			
+
 			return Calendar::$MYSQLI->query($query);
 		} // end function updateField
-		
+
 		public function deleteField() {
 			$ID = $this->returnID();
-			
+
 			$query = "DELETE FROM reminders WHERE ReminderID = '$ID'";
-			
+
 			return Calendar::$MYSQLI->query($query);
 		} // end function deleteField
 	}
