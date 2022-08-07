232909020b66f8d30d168e1fa5ac1740e407bf5e
Hassan Tarawally <contact@htarawally.co.uk>
2022-08-07 15:12:44 +0100

    Update Category.class.php

--- Classes/Category.class.php
+++ Classes/Category.class.php
@@ -1,20 +1,24 @@
 <?php
-	// Abstract class that provides everything 
-	// needed for anything that is basically a category 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+	// Abstract class that provides everything
+	// needed for anything that is basically a category
 
 	abstract class Category extends SavedItem {
 		private $name; // the name for this category i.e. for a company, it would be the company name
-		
+
 		public function __construct($id, $name, $updated) {
 			parent::__construct($id, $updated);
-			
+
 			$this->setCatName($name);
 		} // end constructor
-		
+
 		public final function setCatName($catName) {
 			if(is_string($catName)) {
 				$catName = trim(htmlentities(strip_tags($catName), ENT_QUOTES));
-				
+
 				if(strlen($catName) < 255 && strlen($catName) >= 2) {
 					$this->name = $catName;
 				}
@@ -22,7 +26,7 @@
 			}
 			else throw new InvalidArgumentException("The category name should be a string value");
 		} // end function setCatName
-		
+
 		public final function returnCatName() {
 			return $this->name;
 		} // end function returnCatName
