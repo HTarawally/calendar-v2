9d34a13f4b74ba954d8f2ce4d1ea97b5f94a2b9d
Hassan Tarawally <contact@htarawally.co.uk>
2022-08-07 15:13:11 +0100

    Update Company.class.php

--- Classes/Company.class.php
+++ Classes/Company.class.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 	// Company class that encapsulates the data for companies
 	// modifies, inserts, deletes and creates new companies
 
@@ -6,30 +10,30 @@
 		public function __construct($id, $name, $updated) {
 			parent::__construct($id, $name, $updated);
 		} // end constructor
-		
+
 		public function insertIntoDatabase() {
 			$name = $this->returnCatName();
 
 			$query = "INSERT INTO companies (CompanyName)
 				VALUES ('$name')";
-			
+
 			return Calendar::$MYSQLI->query($query);
 		} // end function insertIntoDatabase
-		
+
 		public function updateField() {
 			$name = $this->returnCatName();
 			$ID = $this->returnID();
-			
+
 			$query = "UPDATE companies SET CompanyName = '$name' WHERE CompanyID = '$ID'";
-				
+
 			return Calendar::$MYSQLI->query($query);
 		} // end function updateField
-		
+
 		public function deleteField() {
 			$ID = $this->returnID();
-			
+
 			$query = "DELETE FROM companies WHERE CompanyID = '$ID'";
-			
+
 			return Calendar::$MYSQLI->query($query);
 		} // end function deleteField
 	}
